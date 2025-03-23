ff15a8b2e7e49ee05892c79ca1c30368f0baada4
allen <294931812@qq.com>
2025-03-23 22:46:16 +0800

    refactor(src): 调整 CSS 文件的导入顺序
    -将 index.css 文件的导入从底部移动到顶部
    - 优化了代码结构，提高了代码的可读性和维护性

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
+import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes.jsx';
